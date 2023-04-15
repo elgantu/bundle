4ae0dd9a1b9d281ba0c2f2d59753375341f95625
elgantu <49722051+elgantu@users.noreply.github.com>
2023-04-15 04:16:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@
                             video.srcObject = stream;
                             mediaStream = stream;
 
-//                             video.play();
+                            video.play();
 
                             startStopButton.setAttribute('data-play', true);
                             initialApp.startVideo(document.getElementById('webcam'), document.getElementById('abc2'), attentionElement, faces, active_faces)
